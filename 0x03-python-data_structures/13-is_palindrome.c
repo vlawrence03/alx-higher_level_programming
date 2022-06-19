6e81524e80beffc9675b9e01649e9a6ca93489d1
vlawrence03 <99636434+vlawrence03@users.noreply.github.com>
2022-06-19 05:25:53 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
-* list_len - finds no. of elements ina linked list.
-* @h: pointer to linked list.
-*
-* Return: number of elements in linked list.
-*/
-size_t list_len(listint_t *h)
+ * list_length - Get the length of a linked list
+ * @head: linked list head
+ * Return: lenght list
+ */
+int list_length(listint_t **head)
 {
-	size_t  nodes = 0;
+	int i;
+	listint_t *curr = *head;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
+	for (i = 0; curr != NULL; i++)
+		curr = curr->next;
+	return (i);
 }
-
 /**
-* is_palindrome - checks if a singly linked list is a palindrome.
-* @head: double pointert to head of d-list.
-*
-* Return: 1 if palindrome, 0 otherwise.
-*/
+ * reversed - function that reversed a linked list
+ * @head: double pointer
+ * Return: Address of the new element
+ */
+listint_t *reversed(listint_t **head)
+{
+	listint_t *prev = NULL, *curr = *head, *next = NULL;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*head = prev;
+	return (*head);
+}
+/**
+ * is_palindrome - checks if a linked list is palindrome
+ * @head: linked list head
+ * Return: 1 if is palindrome or 0 if is not palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-	int *nArr, i = 0, j = 0, len = 0;
-	listint_t *temp;
+	int len = list_length(head);
+	int half_len = len / 2;
+	int i, if_palindrome = 1;
+	listint_t *reversed_head;
+	listint_t *current = *head;
+	listint_t *current_reversed;
 
-	if (*head == NULL)
-		return (1);
-	temp = *head;
-	len = list_len(temp);
-	nArr = (int *)malloc(sizeof(int) * len);
-	if (nArr == NULL)
-		return (2);
-	temp = *head;
-	while (temp != NULL)
+	reversed_head = *head;
+	for (i = 0; i < half_len; i++)
+		reversed_head = reversed_head->next;
+	if (len % 2 != 0)
+		reversed_head = reversed_head->next;
+	reversed_head = reversed(&reversed_head);
+	current_reversed = reversed_head;
+	current = *head;
+	while (current_reversed != NULL)
 	{
-		nArr[j] = temp->n;
-		j++;
-		temp = temp->next;
+		if (current_reversed->n != current->n)
+			if_palindrome = 0;
+		current_reversed = current_reversed->next;
+		current = current->next;
 	}
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		if (nArr[i] != nArr[j])
-			return (0);
-	}
-	return (1);
+	return (if_palindrome);
 }
