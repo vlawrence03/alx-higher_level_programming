9e96fd23bba3c482dd52a01099f5e0fe155b087a
vlawrence03 <99636434+vlawrence03@users.noreply.github.com>
2022-06-19 13:18:07 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
 
-if number < 0:
-    last_digit = ((number * -1) % 10) * -1
-else:
-    last_digit = number % 10
-if last_digit > 5:
-    print("Last digit of {0:d} is {1:d} and is greater than 5"
-        .format(number, last_digit))
-elif last_digit == 0:
-    print("Last digit of {0:d} is {1:d} and is 0"
-        .format(number, last_digit))
-elif last_digit < 6 and not 0:
-    print("Last digit of {0:d} is {1:d} and is less than 6 and not 0"
-        .format(number, last_digit))
+
+def get_randn():
+    number = random.randint(-10000, 10000)
+    return number
+
+
+def get_last_digit(number):
+    if number >= 0:
+        return number % 10
+    else:
+        if (number % 10) != 0:
+            return -10 + (number % 10)
+        else:
+            return 0
+
+
+def print_last_digit(last_digit):
+    print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
+    if last_digit > 5:
+        print('{}'.format('and is greater than 5'))
+    elif last_digit == 0:
+        print('{}'.format('and is 0'))
+    elif last_digit < 6 and last_digit != 0:
+        print('{}'.format('and is less than 6 and not 0'))
+
+
+if __name__ == '__main__':
+    number = get_randn()
+    last_digit = get_last_digit(number)
+    print_last_digit(last_digit)
